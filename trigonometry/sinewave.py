98dd9dc364969b09b86517ad5e0c6f80bacee64b
Manish <mnsh6313@gmail.com>
2023-03-12 16:59:56 +0545

    fix: shifted the graph down for more clear view

--- trigonometry/sinewave.py
+++ trigonometry/sinewave.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
 
     # Loop till 0 to 2*pi which is one complete revolution of the circle with step 0.01
     for angle in np.arange(0, 2 * math.pi, 0.01):
-        x = angle * 200   # horizontal scaling by 200
-        y = math.sin(angle) * 200  # vertical scaling by 200
+        x = angle * 100   # horizontal scaling by 200
+        y = (math.sin(angle) * 100) + 500  # vertical scaling by 200 and shifting it down by 500
         canvas.create_rectangle(x, y, x+5, y+5, fill='black')
     canvas.pack()
     mainloop()
